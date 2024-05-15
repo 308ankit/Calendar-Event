9eb885570b800d8521bc1589d3cc19875bfd8990
Ankit Singh <148118485+308ankit@users.noreply.github.com>
2024-05-15 14:51:51 +0530

    Add files via upload

---
+++ 4a-cal-page.php
@@ -0,0 +1,62 @@
+<?php
+// (A) DAYS MONTHS YEAR
+$months = [
+  1 => "January", 2 => "Febuary", 3 => "March", 4 => "April",
+  5 => "May", 6 => "June", 7 => "July", 8 => "August",
+  9 => "September", 10 => "October", 11 => "November", 12 => "December"
+];
+$monthNow = date("m");
+$yearNow = date("Y"); ?>
+ 
+<!-- (B) PERIOD SELECTOR -->
+<div id="calHead">
+  <div id="calPeriod">
+    <input id="calToday" type="button" value="&#9733;">
+    <input id="calBack" type="button" class="mi" value="&lt;">
+    <select id="calMonth"><?php foreach ($months as $m=>$mth) {
+      printf("<option value='%u'%s>%s</option>",
+        $m, $m==$monthNow?" selected":"", $mth
+      );
+    } ?></select>
+    <input id="calYear" type="number" value="<?=$yearNow?>">
+    <input id="calNext" type="button" class="mi" value="&gt;">
+  </div>
+  <input id="calAdd" type="button" value="+">
+</div>
+ 
+<!-- (C) CALENDAR WRAPPER -->
+<div id="calWrap">
+  <div id="calDays"></div>
+  <div id="calBody"></div>
+</div>
+ 
+<!-- (D) EVENT FORM -->
+<dialog id="calForm"><form method="dialog">
+  <div id="evtCX">X</div>
+  <h2 class="evt100">CALENDAR EVENT</h2>
+  <div class="evt50">
+    <label>Start</label>
+    <input id="evtStart" type="datetime-local" required>
+  </div>
+  <div class="evt50">
+    <label>End</label>
+    <input id="evtEnd" type="datetime-local" required>
+  </div>
+  <div class="evt50">
+    <label>Text Color</label>
+    <input id="evtColor" type="color" value="#000000" required>
+  </div>
+  <div class="evt50">
+    <label>Background Color</label>
+    <input id="evtBG" type="color" value="#ffdbdb" required>
+  </div>
+  <div class="evt100">
+    <label>Event</label>
+    <input id="evtTxt" type="text" autocomplete="off" required>
+  </div>
+  <div class="evt100">
+    <input type="hidden" id="evtID">
+    <input type="button" id="evtDel" value="Delete">
+    <input type="submit" id="evtSave" value="Save">
+  </div>
+</form></dialog>
